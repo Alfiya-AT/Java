246f9969ee4430e9ceb5341fcad7fde791522ba6
Alfiya-AT <alfiyat391@gmail.com>
2025-11-16 21:16:41 +0530

    This my first commit

---
+++ demmo.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class demmo {
+   public demmo() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 1;
+      System.out.println(var1);
+   }
+}
---
+++ j.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class j {
+   public j() {
+   }
+
+   public static void main(String[] var0) {
+      byte var1 = 1;
+      System.out.println(var1);
+   }
+}

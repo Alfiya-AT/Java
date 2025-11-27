4d8b8cca9ab441082d6d26dea003b84cb81f3802
Alfiya-AT <alfiyat391@gmail.com>
2025-11-28 00:16:16 +0530

    commit

---
+++ LinkedHashSetDemo.java
@@ -0,0 +1,17 @@
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet<String> lhs=new LinkedHashSet<>();
+		lhs.add(null);
+		lhs.add("India");
+		lhs.add("Japan");
+		lhs.add("America");
+
+		System.out.println(lhs);
+		
+	}
+
+}

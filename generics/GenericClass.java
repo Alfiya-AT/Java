fecbe584783d6d1fcb90207751d7ad2475eb02c8
Alfiya-AT <alfiyat391@gmail.com>
2025-11-28 00:53:47 +0530

    commit

---
+++ generics/GenericClass.java
@@ -0,0 +1,14 @@
+package generics;
+
+public class GenericClass<T> {
+	T data;
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+}
---
+++ generics/GenericMethod.java
@@ -0,0 +1,9 @@
+package generics;
+public class GenericMethod {
+	public <E> void displayArrayElements(E[] elements) {
+	   for(E e:elements) {
+		   System.out.println("Element is "+e);
+	   }
+	}
+
+}

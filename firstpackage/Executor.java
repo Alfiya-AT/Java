6afa97c453a957b8f9b91f2f2a33a07dfbb12c7f
Alfiya-AT <alfiyat391@gmail.com>
2025-11-28 00:45:27 +0530

    commit

---
+++ firstpackage/Executor.java
@@ -0,0 +1,24 @@
+package firstpackage;
+
+public class Executor {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Person p1=new Person();
+		p1.setPersonName("Gayatri");
+		
+		p1.display();
+		
+		System.out.println(p1.getPersonName());
+		
+		Person p2=new Person();
+		p2.display();
+		
+		Person p3=new Person();
+		p3.display();
+
+	}
+
+}
